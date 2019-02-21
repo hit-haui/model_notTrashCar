87d52d07a589b8ae25ba8d127d8584fcb81e5f27
vicker <haibuai3@gmail.com>
2019-02-21 21:22:22 +0700

    fix nividia

--- nvidia.py
+++ nvidia.py
@@ -1,5 +1,5 @@
 from keras import optimizers
-from keras.layers import BatchNormalization, Conv2D, Flatten, Dense
+from keras.layers import BatchNormalization, Conv2D, Flatten, Dense, Input
 from keras.models import Sequential
 from keras.optimizers import Adam
 from keras.models import Model
@@ -16,30 +16,38 @@ seq_length = 0
 train_split = 0.8
 val_split = 0.2
 
-input_shape = (66, 200, 3)
+#input_shape = (66, 200, 3)
 # Data generator
 #train_generator, val_generator = get_data_loader(batch_size=batch_size, img_size=img_size)
-train_generator = train_generator(batch_size, seq_length, train_split, seq_train = False)
+train_generator = train_generator(
+    (66, 200 , 3), batch_size, seq_length, train_split)
 
-val_generator = val_generator(batch_size, seq_length, val_split, seq_train = False)
-
+val_generator = val_generator(
+    (66, 200, 3), batch_size, seq_length, val_split)
 # Init the model
-model = Sequential()
-model.add(BatchNormalization(epsilon=0.001, axis=1, input_shape= input_shape))
-model.add(Conv2D(24, (5, 5), padding="valid", strides=(2, 2), activation="relu"))
-model.add(Conv2D(36, (5, 5), padding="valid", strides=(2, 2), activation="relu"))
-model.add(Conv2D(48, (5, 5), padding="valid", strides=(2, 2), activation="relu"))
-model.add(Conv2D(64, (3, 3), padding="valid", strides=(1, 1), activation="relu"))
-model.add(Conv2D(64, (3, 3), padding="valid", strides=(1, 1), activation="relu"))
-model.add(Flatten())
-model.add(Dense(1164, activation='relu'))
-model.add(Dense(100, activation='relu'))
-model.add(Dense(50, activation='relu'))
-model.add(Dense(10, activation='relu'))
-model.add(Dense(1, activation='relu',name='steering'))
-model.add(Dense(1, activation='relu', name='speed'))
 
 
+input_shape = Input(shape = img_size, name='input_shape')
+
+X = input_shape
+
+# model = Sequential()
+X = BatchNormalization(epsilon=0.001, axis=1)(X)
+X = Conv2D(24, (5, 5), padding="valid", strides=(2, 2), activation="relu")(X) 
+X = Conv2D(36, (5, 5), padding="valid", strides=(2, 2), activation="relu")(X)
+X = Conv2D(48, (5, 5), padding="valid", strides=(2, 2), activation="relu")(X)
+X = Conv2D(64, (3, 3), padding="valid", strides=(1, 1), activation="relu")(X)
+X = Conv2D(64, (3, 3), padding="valid", strides=(1, 1), activation="relu")(X)
+X = Flatten()(X)
+X = Dense(1164, activation='relu')(X)
+X = Dense(100, activation='relu')(X)
+X = Dense(50, activation='relu')(X)
+
+steering = Dense(1, activation='relu', name='steering')(X)
+speed = Dense(1, activation='relu', name='speed')(X) 
+model = X 
+
+model = Model(inputs=[input_shape], outputs=[steering, speed])
 
 print(model.summary())
 
@@ -51,6 +59,6 @@ model.compile(optimizer = Adam(lr = learning_rate),
 
 # Train the model
 weight_path = "model/first-{epoch:03d}-{val_loss:.5f}.hdf5"
-model.fit_generator(generator=train_generator, steps_per_epoch=train_generator.n//batch_size, epochs=epochs,
-                              validation_data=val_generator, validation_steps=val_generator.n//batch_size,
+model.fit_generator(generator=train_generator, steps_per_epoch=batch_size, epochs=epochs,
+                              validation_data=val_generator, validation_steps=batch_size,
                               callbacks=get_callback(weight_path=weight_path, batch_size=batch_size))
