e024737b9ac321a495b90f5690a6be233fb90b20
vicker <haibuai3@gmail.com>
2019-03-15 01:26:27 +0700

    Add motionblur

--- custom_augmentation.py
+++ custom_augmentation.py
@@ -81,3 +81,43 @@ class RandomSunFlare(iaa.meta.Augmenter):
 
     def get_parameters(self):
         return [self.abc]
+
+class RandomGravel(iaa.meta.Augmenter):
+
+    def __init__(self, abc=1, name=None, deterministic=False,
+                 random_state=None):
+        """Initialize the augmentator"""
+        super(RandomGravel, self).__init__()
+        self.abc = abc
+
+    def _augment_images(self, images, random_state, parents, hooks):
+        result = images
+
+        for _idx, image in enumerate(images):
+            result[_idx] = motionblur(image)
+
+        return result
+
+    def _augment_keypoints(self, keypoints_on_images, random_state, parents,
+                           hooks):
+        return keypoints_on_images
+
+    def _augment_heatmaps(self, **kwargs):
+        return
+
+    def get_parameters(self):
+        return [self.abc]
+
+def motionblur(img): 
+    h,w,_ = img.shape 
+    #left 
+    augment_blur_left = iaa.MotionBlur(k=15, angle =255, direction=-1.0, order=0) 
+    img_drop_left = img_test[:h,:int(w/2)] 
+    motion_left = augment_blur_left.augment_image(img_drop_left) 
+    #right 
+    augment_blur_right = iaa.MotionBlur(k=15, angle= 105, direction=1.0, order=0) 
+    img_drop_right = img_test[:h,int(w/2):w] 
+    motion_right = augment_blur_right.augment_image(img_drop_right) 
+    ### 
+    img[:h,:int(w/2)] = motion_left img[:h,int(w/2):w] = motion_right 
+    return img
