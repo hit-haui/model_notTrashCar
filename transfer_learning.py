1ed6a1e4f6b035d4f31aee0670bebd168a7ab5b6
linus <linus@cinnamon.is>
2019-03-27 07:24:17 +0700

    Refactor code

--- transfer_learning.py
+++ transfer_learning.py
@@ -1,48 +1,53 @@
-from keras import optimizers
-from keras.layers import BatchNormalization, Conv2D, Flatten, Dense, Input, MaxPooling2D, GlobalMaxPooling2D, Concatenate
-from keras.models import Sequential, load_model
-from keras.optimizers import Adam
-from keras.models import Model
-from config import *
-from common_angle import *
-from keras.applications import ResNet50
 import json
-import cv2
+
+from keras.applications import ResNet50
+from keras.layers import (Concatenate, Dense, GlobalMaxPooling2D, Input)
+from keras.models import Model
+from keras.optimizers import Adam
+
+from common_angle_tflearning import *
+
+# Param world
+batch_size = 32
+img_shape = (224, 224, 3)
+epochs = 700
+data_path = '/home/linus/Desktop/final/'
+total_sample = len(json.loads(
+    open(data_path+'/over_sampled_label.json', 'r').read()))
 
 
-train_generator = generator(type_data='train_generator')
-val_generator = generator(type_data='val_generator')
-# Init the model1
+train_generator = generator(type_data='train_generator',
+                            path=data_path, batch_size=batch_size, img_shape=img_shape)
+val_generator = generator(type_data='val_generator',
+                          path=data_path, batch_size=batch_size, img_shape=img_shape)
 
+# Init the model
 input_img = Input(shape=(img_shape), name='input_shape')
-# X = input_img
-T = Input(shape=([3, ]))
-
-base_model = ResNet50(include_top=False, weights='imagenet',
-                      input_shape=img_shape, input_tensor=input_img)
-x = base_model.output
-
-X = GlobalMaxPooling2D()(x)
-concatenateXT = Concatenate(axis=1)([X, T])
-X = Dense(1024, activation='relu')(concatenateXT)
+input_tf_sign = Input(shape=([3, ]), name='input_traffic_sign')
+cnn_model = ResNet50(include_top=False, weights='imagenet',
+                     input_shape=img_shape, input_tensor=input_img)
+X = cnn_model.output
+X = GlobalMaxPooling2D()(X)
+concat = Concatenate(axis=1)([X, input_tf_sign])
+X = Dense(1024, activation='relu')(concat)
 X = Dense(512, activation='relu')(X)
 X = Dense(64, activation='relu')(X)
 X = Dense(1, activation='relu')(X)
-model = Model(input=[input_img, T], output=[X])
+
+model = Model(input=[input_img, input_tf_sign], output=[X])
+
 for layer in model.layers[:-4]:
     layer.trainable = False
+
 print(model.summary())
 
 
 # Loss and optimizer
-model.compile(optimizer=Adam(),
-              loss='mse')
+model.compile(optimizer=Adam(), loss='mse')
 
-total_sample = len(json.loads(
-    open(path+'/over_sampled_label.json', 'r').read()))
 
 # Train the model
-weight_path = "model/TL_read_data_2chanel-{epoch:03d}-{val_loss:.5f}.hdf5"
+weight_path = "model/resnet50_3chanel-{epoch:03d}-{val_loss:.5f}.hdf5"
 model.fit_generator(generator=train_generator, steps_per_epoch=total_sample//batch_size, epochs=epochs,
                     validation_data=val_generator, validation_steps=batch_size,
                     callbacks=get_callback(weight_path=weight_path, batch_size=batch_size,))
